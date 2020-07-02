784e36bd7f7c30178f788b5328ad5489cf3ce845
Sandeep Kumar Shukla <sandeepini.2012@gmail.com>
2020-07-02 12:57:43 +0530

    6 Commit - Middleware myOwnMiddleware 2.0

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const morgan = require("morgan");
 
 //bring in routes
 const {getPosts} = require("./routes/post");
+const myOwnMiddleware = (req, res, next) =>{
+    console.log("Middleware Applied !!");
+    next();
+};
 
 //middleware
 app.use(morgan("dev"));
+app.use(myOwnMiddleware);
 
 app.get("/",getPosts);
 
